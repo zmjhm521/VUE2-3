ea2773006e591bc1158d23ac8dee90292f5e48c7
zhaomingjie <1441586363@qq.com>
2023-01-07 16:34:53 +0800

    寒假第五天

---
+++ 19单文件组件/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <school></school>
+    </div>
+</template>
+
+<script>
+import School from './School'
+export default {
+    name: 'App',
+    components: {
+        School
+    }
+}
+</script>
+
+<style>
+
+</style>
